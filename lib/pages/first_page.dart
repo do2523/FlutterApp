23c79c09c1403ae3bfd198f105f2989c032fa2db
do2523 <do262431@gmail.com>
2025-04-13 01:00:55 -0400

    Create a Drawer Widget for hamburger button

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -7,16 +7,38 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Google Docs"),
-      backgroundColor: Colors.blue),
-      body: Center(
-          child: ElevatedButton(
-            child: Text("Go to Gemini"),
-            onPressed: () {
-              Navigator.pushNamed(context, '/secondpage');
-            },
-          )
+      appBar: AppBar(title: Text("Google Docs")),
+      drawer: Drawer(
+        backgroundColor: Colors.blue,
+        child: Column(
+          children: [
+            DrawerHeader(
+              child: Icon(
+                Icons.star,
+                size: 48
+              ),
+            ),
+
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("H O M E"),
+              onTap: () {
+                Navigator.pushNamed(context, '/homepage');
+              }
+            ),
+
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("S E T T I N G S"),
+              onTap: () {
+                Navigator.pushNamed(context, '/settingspage');
+              }
+            )
+      
+          ]
+        )
       ),
+      // )
     );
     
   }
