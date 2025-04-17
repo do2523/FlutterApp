eeccb66ba08796dd24912db9b8885b080cc940e6
do2523 <do262431@gmail.com>
2025-04-17 01:57:11 -0400

    Add Slidable Widget

--- lib/pages/components/todo_tile.dart
+++ lib/pages/components/todo_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ToDoTile extends StatelessWidget {
   final String taskName;
@@ -15,23 +16,33 @@ class ToDoTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(25),
-      child: Container(
-        padding: const EdgeInsets.all(24),
-        decoration: BoxDecoration(
-          color: Color(0xff4285F4),
-          border: Border.all(),
-          borderRadius: BorderRadius.circular(20),
-        ),
-        child: Row(
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(), 
           children: [
-            Checkbox(
-              value: taskCompleted, 
-              onChanged: buttonPressed,),
-
-           
-        Text(taskName, 
-        style: TextStyle(decoration: taskCompleted ? TextDecoration.lineThrough : TextDecoration.none)),
-          ],
+            SlidableAction(
+              onPressed: removeTask,
+              icon: Icons.delete,
+              backgroundColor: Colors.red,)
+          ]),
+        child: Container(
+          padding: const EdgeInsets.all(24),
+          decoration: BoxDecoration(
+            color: Color(0xff4285F4),
+            border: Border.all(),
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: taskCompleted, 
+                onChanged: buttonPressed,),
+        
+             
+          Text(taskName, 
+          style: TextStyle(decoration: taskCompleted ? TextDecoration.lineThrough : TextDecoration.none)),
+            ],
+          ),
         ),
       ),
     );
